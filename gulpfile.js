7cbb93671d000fc8a56370de21099baca0591ed9
Zach <zach.smith33@gmail.com>
2017-05-22 10:09:41 -0400

    Add dev dependencies
    Add gitignore file to prevent tracking of eslint config and node_modules directory. Remove bootstrap styling. Add Materialize css and js. Implement gulp to automate minification of css and transpiling and minification of js. Configure gulp/browser-sync dev server.

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var babel = require('gulp-babel');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var filter = require('gulp-filter');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+// Compile LESS files from /less into /css
+gulp.task('less', function () {
+  var f = filter(['**', '!less/mixins.less', '!less/variables.less']);
+  return gulp.src('less/*.less')
+    .pipe(f)
+    .pipe(less())
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function () {
+  return gulp.src('public/css/style.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// gulp.task('babel', function () {
+//   return gulp.src('public')
+//     .pipe(babel({
+//       presets: ['es2015']
+//     }))
+//     .pipe(gulp.dest('public'));
+// });
+
+// Minify JS
+gulp.task('minify-js', function () {
+  return gulp.src('public/js/index.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: 'public'
+  });
+});
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function () {
+  gulp.watch('less/*.less', ['less']);
+  gulp.watch('public/css/*.css', ['minify-css']);
+  gulp.watch('public/js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+});
